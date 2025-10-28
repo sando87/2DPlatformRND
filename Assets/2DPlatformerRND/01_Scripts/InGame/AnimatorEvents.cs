0fba2e1aa317466ef9d73641b59cd9c0a8006159
sando87 <tjdwn0406@gmail.com>
2025-10-28 16:16:36 +0900

    Player 리소스 적용

---
+++ Assets/2DPlatformerRND/01_Scripts/InGame/AnimatorEvents.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AnimatorEvents : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    
+    public void Hit()
+    {
+    }
+
+    public void FootR()
+    {
+    }
+
+    public void FootL()
+    {
+    }
+
+    public void Land()
+    {
+    }
+
+    public void Shoot()
+    {
+    }
+}
