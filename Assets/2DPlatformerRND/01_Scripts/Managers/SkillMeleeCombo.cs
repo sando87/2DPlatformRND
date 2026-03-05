4df06f3bc96e9e27bfcbd1ea8a73a00d6369b872
sando87 <tjdwn0406@gmail.com>
2026-03-05 20:23:42 +0900

    meleecombo attack

---
+++ Assets/2DPlatformerRND/01_Scripts/Managers/SkillMeleeCombo.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using NaughtyAttributes;
+using PahlBit;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SkillMeleeCombo : SkillBase
+{
+    [SerializeField] PlayerStateGeneral SkillMotion;
+    [SerializeField] ProjectileBase MeleePrefab;
+
+    public override bool IsCastable()
+    {
+        // 나중에 추가로 쿨타임이나 스턴같은 경우에 대한 조건 추가
+        return SkillMotion.IsChangable();
+    }
+
+    public override void UpdateSkill()
+    {
+        base.UpdateSkill();
+
+        if (mInput.JustPressed(GetCurrentInputType()) && IsCastable())
+        {
+            // mBaseObj.StateMachine.ChangeState(SkillMotion, (Action)DoFire);
+
+            AnimEventState animInfo = mBaseObj.AnimHelper.PlayAnim(AnimStateNameHash.MeleeA);
+            this.ExConditionCoroutine(() => animInfo.IsFired, () => LOG.trace());
+
+            // 이방식은 어떤 스킬 하나가 여러 모션이나 State를 가지고 있으면 문제가 될 가능성 크다..ㅠㅠ
+            this.ExConditionCoroutine(() => animInfo.IsEnd, () => OnEndSkill());
+        }
+    }
+
+    void OnEndSkill()
+    {
+
+    }
+
+    public override void DoFire()
+    {
+        base.DoFire();
+
+        DoCastSkill();
+    }
+
+    public void DoCastSkill()
+    {
+        // 스킬 오브젝트 생성
+        Vector2 startPos = mBaseObj.Body.Center + new Vector2(transform.right.x, 0);
+        ProjectileBase proj = ProjectileBase.Create(MeleePrefab, startPos, mBaseObj.transform.rotation, mBaseObj.gameObject.layer);
+
+        ApplySkillStatsToProjectile(proj);
+
+        proj.OnHit.AddListener((col) =>
+        {
+            // 충돌 시 처리할 내용
+            Health health = col.ExGetBase().GetComponentInChildren<Health>();
+            if (health != null)
+            {
+                DamageInfo damage = Spec.CalcCurrentDamages();
+                health.GetDamaged(damage);
+
+                AttackResult result = new AttackResult()
+                {
+                    Target = col.ExGetBase(),
+                    IsKilled = health.IsDead,
+                };
+                mBaseObj.GetComponentInChildren<BattleDispatcher>()?.DispatchAttackResult(result);
+            }
+        });
+    }
+
+}
