18393ffb0d0eff7797124746d228ab7bcd0b8bb0
sando87 <tjdwn0406@gmail.com>
2026-03-20 05:41:28 +0900

    Iceball 스킬 수정

--- Assets/2DPlatformerRND/01_Scripts/Managers/SkillIceBall.cs
+++ Assets/2DPlatformerRND/01_Scripts/Managers/SkillIceBall.cs
@@ -12,8 +12,6 @@ public class SkillIceBall : SkillBase
     [SerializeField] PlayerStateGeneral SkillMotion;
     [SerializeField] ProjectileBase ProjectilePrefab;
 
-    List<BaseObject> mTargets = new();
-
     public override bool IsCastable()
     {
         return SkillMotion.IsChangable();
@@ -25,7 +23,6 @@ public class SkillIceBall : SkillBase
 
         if (mInput.JustPressed(GetCurrentInputType()) && IsCastable())
         {
-            mTargets.Clear();
             mBaseObj.StateMachine.TryChangeState(SkillMotion, (Action)DoFire);
         }
     }
@@ -34,8 +31,76 @@ public class SkillIceBall : SkillBase
     {
         base.DoFire();
 
-        DoCastSkill();
+        // DoCastSkill();
+
+        List<BaseObject> mTargets = new();
+        Vector2 startPos = mBaseObj.Body.Center + new Vector2(transform.right.x, 0);
+        FireMultiShot((int)Spec.ProjectileCount, startPos, mBaseObj.transform.rotation, 90, mTargets);
     }
+
+
+    void FireMultiShot(int arrowCount, Vector2 startPos, Quaternion baseRotation, float maxSpreadAngle, List<BaseObject> targets)
+    {
+        if (arrowCount <= 0)
+            return;
+
+        float stepAngle = 10;
+        float totalAngle = stepAngle * (arrowCount - 1);
+        if (totalAngle > maxSpreadAngle)
+        {
+            stepAngle = maxSpreadAngle / (arrowCount - 1);
+        }
+
+        for (int i = 0; i < arrowCount; i++)
+        {
+            float offsetIndex = i - (arrowCount - 1) / 2f;
+            float angle = offsetIndex * stepAngle;
+
+            Quaternion rot = baseRotation * Quaternion.Euler(0f, 0f, angle);
+
+            ProjectileBase proj = ProjectileBase.Create(
+                ProjectilePrefab,
+                startPos,
+                rot,
+                mBaseObj.gameObject.layer
+            );
+
+            ApplySkillStatsToProjectile(proj);
+
+            RegistOnHitEvent(proj, targets);
+        }
+    }
+
+    void RegistOnHitEvent(ProjectileBase proj, List<BaseObject> targets)
+    {
+        proj.OnHit.AddListener((col) =>
+        {
+            // 충돌 시 처리할 내용
+            Health health = col.ExGetCompInBase<Health>();
+            if (health != null)
+            {
+                // 충돌 시 처리할 내용
+                BaseObject target = col.ExGetBase();
+                if (targets.Contains(target))
+                    return;
+
+                targets.Add(target);
+
+                DamageInfo damageInfo = Spec.CalcCurrentDamages();
+                health.GetDamaged(damageInfo);
+
+                AttackResult result = new AttackResult()
+                {
+                    Target = col.ExGetBase(),
+                    IsKilled = health.IsDead,
+                };
+                mBaseObj.GetComponentInChildren<BattleDispatcher>()?.DispatchAttackResult(result);
+            }
+
+            // proj.DoEndProjectile();
+        });
+    }
+
 
     public void DoCastSkill()
     {
@@ -47,13 +112,6 @@ public class SkillIceBall : SkillBase
 
         proj.OnHit.AddListener((col) =>
         {
-            // 충돌 시 처리할 내용
-            BaseObject target = col.ExGetBase();
-            if (mTargets.Contains(target))
-                return;
-
-            mTargets.Add(target);
-
             Health health = col.ExGetCompInBase<Health>();
             if (health != null)
             {
