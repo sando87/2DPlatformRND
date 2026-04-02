df0347ca9dc416dcb1ed5ab1e1b11be3edd38085
sando87 <tjdwn0406@gmail.com>
2026-04-02 14:51:23 +0900

    스킬팝업 창 작업 중

---
+++ Assets/2DPlatformerRND/01_Scripts/UI/UIInputHandler.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PahlBit
+{
+    public class UIInputHandler : MonoBehaviour, IInputHandler
+    {
+        [SerializeField] UIPartsHandler[] _UIParts;
+
+        public UIPartsHandler CurrentSelectedPart { get; private set; }
+
+        public void UpdateUIParts()
+        {
+            _UIParts = GetComponentsInChildren<UIPartsHandler>();
+        }
+
+        public void OnInputUpdate(InputSystemManager inputManager)
+        {
+            if (inputManager.JustPressed(PlayerUnitInputType.UIMove))
+            {
+                Vector2 moveDir = inputManager.GetInputValue<Vector2>(PlayerUnitInputType.UIMove);
+                if (moveDir.magnitude > 0.1f)
+                {
+                    Move(moveDir.normalized);
+                }
+            }
+        }
+
+        void Move(Vector2 dir)
+        {
+            if (_UIParts == null || _UIParts.Length == 0)
+                return;
+
+            GameObject current = EventSystem.current.currentSelectedGameObject;
+            if (current == null)
+            {
+                CurrentSelectedPart = _UIParts[0];
+                EventSystem.current.SetSelectedGameObject(_UIParts[0].gameObject);
+                return;
+            }
+
+            RectTransform currentRect = current.GetComponent<RectTransform>();
+            Vector3 currentPos = currentRect.position;
+
+            UIPartsHandler best = null;
+            float bestScore = float.MaxValue;
+
+            foreach (var btn in _UIParts)
+            {
+                if (btn.gameObject == current) continue;
+
+                Vector3 dirToTarget = btn.transform.position - currentPos;
+
+                // 방향 체크 (위쪽인지 등)
+                if (Vector2.Dot(dir, dirToTarget.normalized) < 0.5f)
+                    continue;
+
+                float distance = dirToTarget.sqrMagnitude;
+
+                if (distance < bestScore)
+                {
+                    bestScore = distance;
+                    best = btn;
+                }
+            }
+
+            if (best != null)
+            {
+                CurrentSelectedPart = best;
+                EventSystem.current.SetSelectedGameObject(best.gameObject);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/2DPlatformerRND/01_Scripts/UI/UIPartsActions.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PahlBit
+{
+    public enum UIActionType { Learn, Use, UnUse, Enforce, Equip, UnEquip, Sell, Buy }
+
+    public class UIPartsActions : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI _FieldName;
+        [SerializeField] TextMeshProUGUI _FieldValue;
+
+        void Awake()
+        {
+        }
+
+        public void SetField(string fieldName, string fieldValue)
+        {
+            _FieldName.text = fieldName;
+            _FieldValue.text = fieldValue;
+        }
+    }
+}
