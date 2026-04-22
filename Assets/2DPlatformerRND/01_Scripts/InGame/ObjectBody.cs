f57bf2bfef4ebc1010714132c2f8f45bc3dd2087
sjlee <sjlee@gmail.com>
2026-04-22 15:04:17 +0900

    얇은지형 경계에서 떨어지는 이슈 개선

--- Assets/2DPlatformerRND/01_Scripts/InGame/ObjectBody.cs
+++ Assets/2DPlatformerRND/01_Scripts/InGame/ObjectBody.cs
@@ -30,6 +30,12 @@ namespace PahlBit
         void Awake()
         {
             mCollider = GetComponent<BoxCollider2D>();
+
+            // 유닛 이동시 Terrain와 얇은지형 경계에서 자꾸 밑으로 떨어지는 버그 수정
+            if (_ThinPlatform != null)
+            {
+                _ThinPlatform.transform.localPosition += new Vector3(0, 0.01f, 0);
+            }
         }
 
         public void LockThinPlatformMomentarily(float duration = 0.2f)
